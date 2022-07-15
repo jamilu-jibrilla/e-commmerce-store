eb9fffdd706d3fddcb3f30e167e954417e5c7349
jamilu jibrilla <jamilujibrilla01@gmail.com>
2022-07-16 00:12:21 +0100

    Added tailwind css and react icons to the project (#8)
    * updating readme
    
    * added tailwind css and react-icons to the project

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,10 +1,9 @@
-
 const Home = () => {
-    return (
-        <>
-            <h1>hello world</h1>
-        </>
-    );
-}
+  return (
+    <>
+      <h1 className="text-3xl font-bold">hello world</h1>
+    </>
+  );
+};
 
 export default Home;
