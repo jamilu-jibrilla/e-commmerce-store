3b07385dc84b56bced464b65be6c0550b9173b1a
jamil <jamilujibrilla01@gmail.com>
2022-07-16 02:21:27 +0100

    updated react router dom to its latest version

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,34 +1,46 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav>
-            <ul className='main-nav'>
+  return (
+    <nav>
+      <ul className="main-nav">
+        <li className="push-left">
+          {" "}
+          <i className="fa fa-bars push-left"></i> Categotries
+          <ul className="categories">
+            <li>Dresses</li>
+            <li>Shirts</li>
+            <li>Jeans</li>
+            <li>Sleepwear</li>
+            <li>Swimwear</li>
+            <li>sportswear</li>
+            <li>Sleepwear</li>
+          </ul>
+        </li>
+        <Link to="/about" className="push-left">
+          <li>Home</li>
+        </Link>
+        <Link to="/contact">
+          <li>Shop</li>
+        </Link>
+        <Link to="/contact">
+          <li>shop Detail</li>
+        </Link>
+        <Link to="/">
+          <li>Contact</li>
+        </Link>
+      </ul>
 
-                <li className='push-left'> <i className="fa fa-bars push-left"></i> Categotries
-                    <ul className='categories'>
-                        <li>Dresses</li>
-                        <li>Shirts</li>
-                        <li>Jeans</li>
-                        <li>Sleepwear</li>
-                        <li>Swimwear</li>
-                        <li>sportswear</li>
-                        <li>Sleepwear</li>
-                    </ul>
-                </li>
-                <Link href="/about" className="push-left"><li>Home</li></Link>
-                <Link href="/contact"><li>Shop</li></Link>
-                <Link href="/contact"><li>shop Detail</li></Link>
-                <Link href="/"><li>Contact</li></Link>
-            </ul>
-
-            <ul className='cart'>
-                <li><i className="fa fa-heart"></i></li>
-                <li className='push-right'><i className="fa fa-shopping-cart"></i></li>
-            </ul>
-
-        </nav>
-    );
-}
+      <ul className="cart">
+        <li>
+          <i className="fa fa-heart"></i>
+        </li>
+        <li className="push-right">
+          <i className="fa fa-shopping-cart"></i>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
